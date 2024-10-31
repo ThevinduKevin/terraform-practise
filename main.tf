13f309789426bd2bbbf7b77f08de253a5981dc3b
ThevinduKevin <thevindukevin@gmail.com>
2024-11-01 00:20:54 +0530

    terraform practise

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource local_file my-pet {
+  filename = ""
+  content = var.file-content[statement1]
+}
---
+++ variable.tf
@@ -0,0 +1,7 @@
+variable file-content {
+    type = map
+    default = {
+        "statement1" = "helloo"
+        "statement2" = "hiiiii"
+    }
+}
