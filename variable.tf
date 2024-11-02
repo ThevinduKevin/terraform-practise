9d3001ec09b0c397666e356d276dd7348dc7d223
ThevinduKevin <thevindukevin@gmail.com>
2024-11-02 10:38:48 +0530

    more practise

--- variable.tf
+++ variable.tf
@@ -1,3 +1,6 @@
+//variable types
+
+//map
 variable file-content {
     type = map
     default = {
@@ -6,6 +9,7 @@ variable file-content {
     }
 }
 
+//list
 variable "prefix" {
   default = ["mr","mrs","sir"]
   type = list(string)
@@ -14,4 +18,40 @@ variable "prefix" {
 variable "prefix" {
   default = ["1","2","3"]
   type = list(number)  //within brackets we can't use number for strings even we have the same variable
+}
+
+//set
+variable "prefi" {
+  default = ["ab","bc","bc"]  //in a set type there shouldn't be a repeated data. this is wrong
+  type = set(string)
+}
+
+//object type is a complex type
+variable "bella" {
+  type = object({
+    name = string
+    color = string
+    age = number
+    food = list(string)
+    favorite_pet = bool
+  })
+
+  default = {
+    name = "bella"
+    color = "brown"
+    age = 7
+    food = [ "fish","chicken" ]
+    favorite_pet = true
+  }
+}
+
+//tuple is like list. but different
+variable "kitty" {
+  type = tuple([ string, number, bool ])
+  default = [ "cat", 7, true ]
+}
+
+variable "kitty" {
+  type = tuple([ string, number, bool ])
+  default = [ "cat", 7, true, "dog" ]  //this is wrong. there is no typ for dog
 }
